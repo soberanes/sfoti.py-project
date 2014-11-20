eab22f1f076dfee43122c4316d1e772bed0bb010
soberanes <paulsoberanes@gmail.com>
2014-11-20 16:06:46 -0600

    Initial commit

---
+++ sfotipy/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'sfotipy.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^tracks/(?P<title>[\w\-]+)/', 'tracks.views.track_view', name = 'track_view'),
+    url(r'^signup/', 'userprofiles.views.signup', name = 'signup'),
+    url(r'^signin/', 'userprofiles.views.signin', name = 'signin'),
+)
